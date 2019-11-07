32a35dae8e2c02bfc57759c77321e0e6fda25e40
cjustinobi <cjustinobi@gmail.com>
2019-11-08 00:46:14 +0100

    ch(nodemon): add nodemon to the workflow

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  googleClientID: '358021494080-22b6ra5kcmctpn21ieaktnncqns5e9nh.apps.googleusercontent.com',
+  googleClientSecret: 'AZPSEAPSuTh2VC1tF7gTjapn'
+}
--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 const express = require('express')
+const passport = require('passport')
+const GoogleStrategy = require('passport-google-oauth20').Strategy
+const { googleClientID, googleClientSecret } = require('./config/keys')
 
 const app = express()
 
-app.get('/', (req, res) => {
-  res.send({name: 'Justin Obi'})
+passport.use(new GoogleStrategy({
+      clientID: googleClientID,
+      clientSecret: googleClientSecret,
+      callbackURL: '/auth/google/callback'
+    }, (aT) => {
+      console.log(aT)
+    })
+)
+
+app.get('/auth/google', passport.authenticate('google', {
+  scope: ['profile', 'email']
+}))
+
+app.get('/auth/google/callback', ((req, res) => {
+  console.log(res)
 })
+)
 
-const PORT = process.env.port || 5000
+const PORT = process.env.PORT || 5000
 
-app.listen(PORT)
+app.listen(PORT, () => console.log(`Listen on port: ${PORT}`))
