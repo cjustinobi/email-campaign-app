c8b79e537e14f22444c4f87957b4388cccd2d270
cjustinobi <cjustinobi@gmail.com>
2019-12-03 02:33:08 +0100

    Fix mobile navigation menu

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,8 +2,20 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { Link} from 'react-router-dom'
 import Payments from './Payments'
+import M from "materialize-css";
+import '../assets/css/style.css'
 
 class Header extends Component{
+
+  componentDidMount() {
+    const options = {
+      inDuration: 250,
+      outDuration: 200,
+      draggable: true
+    };
+    const instance = M.Sidenav.init(this.Sidenav);
+  }
+
   renderContent() {
     switch (this.props.auth) {
       case null:
@@ -13,27 +25,45 @@ class Header extends Component{
       default:
         return [
             <li key="1"><Payments/></li>,
-            <li key="3" style={{ margin: '0 10px' }}>Credits: {this.props.auth.credits}</li>,
-            <li key="2"><a href="/api/logout">Logout</a></li>
+            <li key="3" className="mobile-link" style={{ margin: '0 10px' }}>Credits: {this.props.auth.credits}</li>,
+            <li key="2"><a className="mobile-link" href="/api/logout">Logout</a></li>
           ]
     }
   }
   render() {
     console.log(this.props)
     return (
-        <nav>
-          <div className="nav-wrapper">
-            <Link
-                to={this.props.auth ? '/surveys' : '/'}
-                className="brand-logo left"
-            >
-              Emaily
-            </Link>
-            <ul className="right">
-              {this.renderContent()}
-            </ul>
-          </div>
-        </nav>
+        <div>
+          <nav>
+            <div className="nav-wrapper">
+              <Link
+                  to={this.props.auth ? '/surveys' : '/'}
+                  className="brand-logo left"
+              >
+                Emaily
+              </Link>
+              <div>
+                <ul
+                    ref={Sidenav => {this.Sidenav = Sidenav;}}
+                    id="slide-out"
+                    className="sidenav mobile-nav"
+                >
+                  <span data-target="slide-out" className="cancel-icon sidenav-close">&#10060;</span>
+                  {this.renderContent()}
+                </ul>
+                <a data-target="slide-out" className="right sidenav-trigger">
+                  <div className="menu-icon"></div>
+                  <div className="menu-icon"></div>
+                  <div className="menu-icon"></div>
+                </a>
+              </div>
+              <ul className="right hide-on-med-and-down">
+                {this.renderContent()}
+              </ul>
+            </div>
+          </nav>
+        </div>
+
     )
   }
 }
